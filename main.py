14ee0903b90eab81dda7338c6e5e6207deb13a03
Lucasmogsan <lucassandby@gmail.com>
2023-03-01 13:48:00 +0100

    finished tutorial

--- main.py
+++ main.py
@@ -1,40 +1,44 @@
 import pygame
-from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE
-from checkers.board import *
-from checkers.piece import *
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED
+from checkers.game import Game
 
 FPS = 60
 
-WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
 
-def get_row_col_from_mouse(position):
-    x, y = position
+
+def get_row_col_from_mouse(pos):
+    x, y = pos
     row = y // SQUARE_SIZE
     col = x // SQUARE_SIZE
     return row, col
 
+
 def main():
     run = True
     clock = pygame.time.Clock()
-    board = Board()
-    
+    game = Game(WIN)
+
     while run:
         clock.tick(FPS)
-        
+
+        if game.winner() != None:
+            print('Winner: ', game.winner())
+            run = False
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                position = pygame.mouse.get_pos()
-                row, col = get_row_col_from_mouse(position)
-                piece = board.get_piece(row, col)
-                board.move_piece(piece, 0, 2)
-                
-        board.draw_board(WINDOW)
-        pygame.display.update()
-        
-    pygame.quit()
             
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                game.select(row, col)
+
+        game.update()
+    
+    pygame.quit()
+
+
 main()
